b9c36e728b4d81afd62e37f519f4bc8a48af76e5
William May <wjrm500@gmail.com>
2022-09-17 09:09:00 +0100

    Writing algorithm

--- classes.py
+++ classes.py
@@ -17,15 +17,20 @@ class State:
 
     def __init__(self, state: State = None) -> None:
         self.state = state or [[3, 3], [0, 0]]
+    
+    def __eq__(self, state: State) -> bool:
+        return self.state == state.state
 
     def is_goal_state(self) -> bool:
         return self.state == self.GOAL_STATE
     
     def next_state(self, action) -> State:
-        person, river_side = action[0].value, action[1].value
+        first_person, second_person, river_side = action
         new_state = copy.deepcopy(self.state)
-        new_state[river_side][person] -= 1
-        new_state[abs(river_side - 1)][person] += 1
+        for person in [first_person, second_person]:
+            if person is not None:
+                new_state[river_side.value][person.value] -= 1
+                new_state[abs(river_side.value - 1)][person.value] += 1
         return State(new_state)
     
     def is_valid_state(self) -> bool:
@@ -39,12 +44,10 @@ class State:
         return no_negative_counts and valid_left_side and valid_right_side
     
     def get_possible_actions(self) -> List: # Regardless of valid
-        return [
-            (Person.MISSIONARY, TravelDirection.TO_END),
-            (Person.MISSIONARY, TravelDirection.TO_START),
-            (Person.CANNIBAL, TravelDirection.TO_END),
-            (Person.CANNIBAL, TravelDirection.TO_START)
-        ]
+        first_person_opts = [Person.MISSIONARY, Person.CANNIBAL]
+        second_person_opts = [Person.MISSIONARY, Person.CANNIBAL, None]
+        travel_direction_opts = [TravelDirection.TO_START, TravelDirection.TO_END]
+        return list(itertools.product(first_person_opts, second_person_opts, travel_direction_opts))
     
     def get_valid_actions(self) -> List:
         return [action for action in self.get_possible_actions() if self.next_state(action).is_valid_state()]
--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-from classes import Person, TravelDirection, State
+from classes import State
 
-action = (Person.MISSIONARY, TravelDirection.TO_END)
-state = State()
-print(state.get_valid_actions())
+test_state = State()
+
+# for action in test_state.get_possible_actions():
+    # print(action)
+
+def breadth_first_search(state: State) -> State:
+    explored, frontier = [], []
+    while not state.is_goal_state():
+        explored.append(state)
+        for valid_action in state.get_valid_actions():
+            next_state = state.next_state(valid_action)
+            if next_state not in explored and next_state not in frontier:
+                frontier.append(next_state)
+        if len(frontier) == 0:
+            return None
+        state = frontier.pop(0)
+    return state
+
+print(breadth_first_search(test_state).state)
